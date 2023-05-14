3550647c78b20c72726edbb44652d85937f6f0b6
sibmaks <sibmaks@vk.com>
2023-05-14 15:00:20 +0300

    Add SlotsScreenHandler

---
+++ src/main/java/com/github/sib_energy_craft/sec_utils/screen/SlotsScreenHandler.java
@@ -0,0 +1,63 @@
+package com.github.sib_energy_craft.sec_utils.screen;
+
+import com.github.sib_energy_craft.sec_utils.screen.slot.SlotGroupsMeta;
+import com.github.sib_energy_craft.sec_utils.screen.slot.SlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author sibmaks
+ * @since 0.0.14
+ */
+public abstract class SlotsScreenHandler extends ScreenHandler {
+    protected final SlotGroupsMeta slotGroupsMeta;
+
+    protected SlotsScreenHandler(@Nullable ScreenHandlerType<?> type,
+                                 int syncId,
+                                 @NotNull SlotGroupsMeta slotGroupsMeta) {
+        super(type, syncId);
+        this.slotGroupsMeta = slotGroupsMeta;
+    }
+
+    /**
+     * Insert item into specific slot type or in otherwise one
+     *
+     * @param slotStack slot to insert
+     * @param to most priority slot type
+     * @param otherwise other slot types to insert
+     * @return true - slot was insert, false - otherwise
+     */
+    public boolean insertItem(@NotNull ItemStack slotStack,
+                              @NotNull SlotType to,
+                              @NotNull SlotType ... otherwise) {
+        if(insertItem(slotStack, to)) {
+            return true;
+        }
+        for (var slotType : otherwise) {
+            if(insertItem(slotStack, slotType)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Insert item into specific slot type
+     *
+     * @param slotStack slot to insert
+     * @param to slot type
+     * @return true - slot was insert, false - otherwise
+     */
+    public boolean insertItem(@NotNull ItemStack slotStack,
+                              @NotNull SlotType to) {
+        var slotGroupMeta = this.slotGroupsMeta.getSlotGroupMeta(to);
+        if(slotGroupMeta == null) {
+            return false;
+        }
+        var globalRange = slotGroupMeta.getGlobalRange();
+        return insertItem(slotStack, globalRange.minIndex(), globalRange.maxIndex() + 1, false);
+    }
+}
